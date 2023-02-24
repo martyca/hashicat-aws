f7195613390fe7113b6e430f46de484ed8076bba
martyca <martin@caarels.com>
2023-02-24 00:35:42 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
-  source  = "app.terraform.io/leaven-control_tower-caarels/s3-bucket/aws/"
+  source  = "app.terraform.io/leaven-control_tower-caarels/s3-bucket/aws//"
   bucket_prefix = "martincaarels"
   version = "2.8.0"
 }
